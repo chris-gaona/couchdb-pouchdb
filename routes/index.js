cd95c6ee50443bfcd2b3ea3d4b9a880710dab5b9
Chris Gaona <thebeloved88@sbcglobal.net>
2016-11-10 14:21:54 -0800

    Starts working with pouchdb documents; creates new kittens local db

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,36 @@
 var express = require('express');
 var router = express.Router();
 
+// var PouchDB = require('pouchdb');
+// var localdb = new PouchDB('kittens');
+// var remotedb = new PouchDB('http://localhost:5984/kittens');
+
+// localdb.info().then(function (info) {
+//   console.log(info);
+// });
+
+// var doc = {
+//   "_id": "mittens",
+//   "name": "Mittens",
+//   "occupation": "kitten",
+//   "age": 3,
+//   "hobbies": [
+//     "playing with balls of yarn",
+//     "chasing laser pointers",
+//     "lookin' hella cute"
+//   ]
+// };
+// localdb.put(doc);
+
+// remotedb.info().then(function (info) {
+//   console.log(info);
+// });
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  // localdb.get('mittens').then(function (doc) {
+  //   console.log(doc);
+  // });
   res.render('index', { title: 'Express' });
 });
 
